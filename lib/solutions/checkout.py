5a309e548e59d17a852218475d039deb28688789
sbx_user1059 <sbx_user1059@ip-10-11-80-184.eu-west-2.compute.internal>
2018-02-23 16:41:05 +0000

    Fri Feb 23 16:41:05 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -3,4 +3,8 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    sku_to_count = {}
+    for sku in ["A", "B", "C", "D"]:
+        sku_to_count[sku] = skus.count(sku)
+        
+    
